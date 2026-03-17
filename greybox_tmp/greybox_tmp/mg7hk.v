8b95446e245a3ee66227ee9201c437ce1495d983
Talha Shahzad <talhashahzad12345@gmail.com>
2026-03-17 01:46:17 -0600

    grey

---
+++ greybox_tmp/greybox_tmp/mg7hk.v
@@ -0,0 +1,60 @@
+//lpm_divide CBX_SINGLE_OUTPUT_FILE="ON" LPM_DREPRESENTATION="UNSIGNED" LPM_HINT="LPM_REMAINDERPOSITIVE=TRUE" LPM_NREPRESENTATION="UNSIGNED" LPM_TYPE="LPM_DIVIDE" LPM_WIDTHD=8 LPM_WIDTHN=32 denom numer quotient remain
+//VERSION_BEGIN 24.1 cbx_mgl 2025:03:05:20:07:01:SC cbx_stratixii 2025:03:05:20:06:36:SC cbx_util_mgl 2025:03:05:20:06:36:SC  VERSION_END
+// synthesis VERILOG_INPUT_VERSION VERILOG_2001
+// altera message_off 10463
+
+
+
+// Copyright (C) 2025  Altera Corporation. All rights reserved.
+//  Your use of Altera Corporation's design tools, logic functions 
+//  and other software and tools, and any partner logic 
+//  functions, and any output files from any of the foregoing 
+//  (including device programming or simulation files), and any 
+//  associated documentation or information are expressly subject 
+//  to the terms and conditions of the Altera Program License 
+//  Subscription Agreement, the Altera Quartus Prime License Agreement,
+//  the Altera IP License Agreement, or other applicable license
+//  agreement, including, without limitation, that your use is for
+//  the sole purpose of programming logic devices manufactured by
+//  Altera and sold by Altera or its authorized distributors.  Please
+//  refer to the Altera Software License Subscription Agreements 
+//  on the Quartus Prime software download page.
+
+
+
+//synthesis_resources = lpm_divide 1 
+//synopsys translate_off
+`timescale 1 ps / 1 ps
+//synopsys translate_on
+module  mg7hk
+	( 
+	denom,
+	numer,
+	quotient,
+	remain) /* synthesis synthesis_clearbox=1 */;
+	input   [7:0]  denom;
+	input   [31:0]  numer;
+	output   [31:0]  quotient;
+	output   [7:0]  remain;
+
+	wire  [31:0]   wire_mgl_prim1_quotient;
+	wire  [7:0]   wire_mgl_prim1_remain;
+
+	lpm_divide   mgl_prim1
+	( 
+	.denom(denom),
+	.numer(numer),
+	.quotient(wire_mgl_prim1_quotient),
+	.remain(wire_mgl_prim1_remain));
+	defparam
+		mgl_prim1.lpm_drepresentation = "UNSIGNED",
+		mgl_prim1.lpm_nrepresentation = "UNSIGNED",
+		mgl_prim1.lpm_type = "LPM_DIVIDE",
+		mgl_prim1.lpm_widthd = 8,
+		mgl_prim1.lpm_widthn = 32,
+		mgl_prim1.lpm_hint = "LPM_REMAINDERPOSITIVE=TRUE";
+	assign
+		quotient = wire_mgl_prim1_quotient,
+		remain = wire_mgl_prim1_remain;
+endmodule //mg7hk
+//VALID FILE
